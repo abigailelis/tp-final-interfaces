4e351bd9a42cae63eb60b498639d88c5f9d1a623
Abigail Samanta Elis <abigailelis.s@hotmail.com>
2024-11-18 19:10:06 -0300

    pagina de donaciones

--- main.js
+++ main.js
@@ -8,42 +8,56 @@ let servicios = document.getElementById('href-servicios');
 let nosotros = document.getElementById('href-nosotros');
 let login = document.getElementById('href-login');
 let publicar = document.getElementById('href-publicar');
+let donar = document.getElementById('btn-donar');
+let contenido_dinamico = document.getElementById('contenido-dinamico');
 
 inicio.addEventListener('click', function (event) {
     event.preventDefault();
+    contenido_dinamico.classList.remove('bg-img');
     cargarContenido('inicio.html');
 })
 
 contacto.addEventListener('click', function (event) {
     event.preventDefault();
+    contenido_dinamico.classList.add('bg-img');
     cargarContenido('contacto.html');
 })
 
 servicios.addEventListener('click', function (event) {
     event.preventDefault();
+    contenido_dinamico.classList.remove('bg-img');
     cargarContenido('servicios.html');
 })
 
 nosotros.addEventListener('click', function (event) {
     event.preventDefault();
+    contenido_dinamico.classList.remove('bg-img');
     cargarContenido('nosotros.html');
 })
 
 login.addEventListener('click', function (event) {
     event.preventDefault();
+    contenido_dinamico.classList.add('bg-img');
     cargarContenido('login.html');
 })
 
 publicar.addEventListener('click', function (event) {
     event.preventDefault();
+    contenido_dinamico.classList.add('bg-img');
     cargarContenidoBtn('administracion.html', 'btn-cargar-imagen');
+})
+
+donar.addEventListener('click', function (event) {
+    event.preventDefault();
+    contenido_dinamico.classList.remove('bg-img');
+    cargarContenido('donacion.html');
 })
 
 function cargarContenido(html) {
     fetch(html)
         .then(response => response.text())
         .then(data => {
-            document.getElementById('contenido-dinamico').innerHTML = data;
+            contenido_dinamico.innerHTML = data;
         })
         .catch(error => console.error('Error al cargar el contenido:', error));
 }
@@ -52,7 +66,7 @@ function cargarContenidoBtn(html, btn) {
     fetch(html)
         .then(response => response.text())
         .then(data => {
-            document.getElementById('contenido-dinamico').innerHTML = data;
+            contenido_dinamico.innerHTML = data;
             let btnCargarImg = document.getElementById(btn);
 
             btnCargarImg.addEventListener('click', function () {
